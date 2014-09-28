9a1ad0fb00528106c539f1161802927673bc793e
Erik Trautman <erik.trautman@gmail.com>
2014-09-27 20:01:16 -0700

    set up and render board model

--- app/controllers/tic_tac_toe_controller.rb
+++ app/controllers/tic_tac_toe_controller.rb
@@ -3,7 +3,10 @@ class TicTacToeController < ApplicationController
   # Set up a brand new game board
   def new
     @current_player = 'X'
-    save_player(@current_player)
+    @board_arr = Board.blank_board
+
+    save_player
+    save_board_arr
 
     render :game_board
   end
@@ -11,32 +14,43 @@ class TicTacToeController < ApplicationController
   # Handle submitted moves
   # Assume successful move for now
   def make_move
-    @current_player = current_player
+    @current_player = retrieve_current_player
+    @board_arr = retrieve_board_arr
 
     # TODO: add logic here to play the turn
 
-    @current_player = switched_player(current_player)
-    save_player(@current_player)
+    switch_player
+    save_player
+    save_board_arr
 
     render :game_board
   end
 
-
   private
 
-  def switched_player(player)
-    player == 'X' ? 'O' : 'X'
+  # switch the current player
+  def switch_player
+    @current_player = (@current_player == 'X' ? 'O' : 'X')
   end
 
   # pull the current player from the session
-  def current_player
+  def retrieve_current_player
     session[:current_player]
   end
 
-  # save the specified player as the current player 
-  # in the session
-  def save_player(player)
-    session[:current_player] = player
+  # save the current player in the session
+  def save_player
+    session[:current_player] = @current_player
   end 
+
+  # save the board into the session
+  def save_board_arr
+    session[:saved_board_arr] = @board_arr
+  end
+
+  # pull the board from the session
+  def retrieve_board_arr
+    session[:saved_board_arr]
+  end
 
 end
---
+++ app/models/board.rb
@@ -0,0 +1,13 @@
+class Board
+  attr_reader :board_arr
+
+  # initialize board
+  def initialize(board_arr = nil)
+      # set up blank data structure if none given
+      @board_arr = board_arr || blank_board
+  end
+
+  def self.blank_board
+    Array.new(3){Array.new(3)}
+  end
+end
